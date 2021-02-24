fa1b75ab076f856091c8233c3e5d868b60ab9192
Gabriel <gabrieldiniz54@gmail.com>
2021-02-24 13:21:32 -0300

    Versao final do trabalho

--- src/funcoes.c
+++ src/funcoes.c
@@ -8,13 +8,13 @@
 
 
 float calc_area(int numero_threads, int numero_trapezios) {
-    float* v[numero_trapezios];
+    float v[numero_trapezios];
     float h, area_total;
     float a = 0.0;
     float b = 10;
 
     h = (b - a) / numero_trapezios;
-    area_total = (f1(a) + f1(b))/2;
+    area_total = (f2(a) + f2(b))/2;
 
     // Aqui eu casto explicitamente para float para a divisão ser um float. A divisão de dois inteiros
     // Gera um inteiro e a função ceil não consegue fazer seu aredondamento corretamente se a divisão
@@ -28,8 +28,9 @@ float calc_area(int numero_threads, int numero_trapezios) {
         v);
 
     for (int i = 1; i < numero_trapezios; ++i) {
-        float x_i = a + i * h;
-        area_total += f1(x_i);
+        // float x_i = a + i * h;
+        // printf("%f\n", v[i]);
+        area_total += v[i];
     }
 
     area_total = h * area_total;
--- src/threads.c
+++ src/threads.c
@@ -16,13 +16,11 @@ typedef struct {
 
 void* calc_trapezios_t(void* struct_info) {
     InfoParaCalc info = *((InfoParaCalc*) struct_info);
-    printf("Entrei aqui %d %d\n", info.inicio, info.fim);
 
-    for (int i = info.inicio; i < info.fim - 1; ++i) {
+    for (int i = info.inicio; i < info.fim; ++i) {
         float x_i = info.a + i * info.h;
-        info.v[i] = f1(x_i);
+        info.v[i] = f2(x_i);
     }
-    printf("Sai da thread com %d %d\n", info.inicio, info.fim);
 
     pthread_exit(NULL);
 }
@@ -31,8 +29,8 @@ void* calc_trapezios_t(void* struct_info) {
 void calc_trapezios_via_threads(int numero_threads, int numero_trapezios, float a, float h, float* v) {
     int qtd_trapezios_por_thead = ceil((float) numero_trapezios/numero_threads);
     int qtd_trapezios_calculados = 0;
-    int i = 1;
-    pthread_t ids[numero_threads + 1];
+    int i = 0;
+    pthread_t ids[numero_threads];
     InfoParaCalc infos[numero_threads + 1];
 
 
@@ -46,15 +44,16 @@ void calc_trapezios_via_threads(int numero_threads, int numero_trapezios, float 
         infos[i].v = v;
         infos[i].inicio = qtd_trapezios_vez_anterior;
         infos[i].fim = (qtd_trapezios_calculados > numero_trapezios) ?
-                       qtd_trapezios_calculados - (qtd_trapezios_calculados - numero_trapezios - 1) :
+                       qtd_trapezios_calculados - (qtd_trapezios_calculados - numero_trapezios) :
                        qtd_trapezios_calculados;
 
+        printf("%d %d\n", infos[i].inicio, infos[i].fim);
         pthread_create(&ids[i], NULL, calc_trapezios_t, (void*)&infos[i]);
 
         ++i;
     }
 
-    // for (int j = 0; j < numero_threads; ++j) {
-    //     pthread_join(ids[j], NULL);
-    // }
+    for (int j = 0; j < numero_threads; ++j) {
+        pthread_join(ids[j], NULL);
+    }
 }
